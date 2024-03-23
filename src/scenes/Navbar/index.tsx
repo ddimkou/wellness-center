77083aa0a0a9b533e6a8cd884dd107cae78c7bd5
ddimkou <ddimkou@outlook.com>
2024-03-23 11:34:44 +0200

    mobile navbar

--- src/scenes/Navbar/index.tsx
+++ src/scenes/Navbar/index.tsx
@@ -54,13 +54,47 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
             </div>) :
             // mobile screen
              (
-             <button className="rounded-full bg-cyan" onClick={()=>setIsMenuToggled(!isMenuToggled)}>
+             <button className="rounded-full bg-cyan hover:bg-primary-500" onClick={()=>setIsMenuToggled(!isMenuToggled)}>
               <Bars3Icon className="h-7 w-7 p-1 text-white"/>
              </button>
             )}
           </div>
         </div>
       </div>
+      {/* MOBILE MENU toggle*/}
+      {!isAboveMediumScreens && isMenuToggled && (
+        <div className="fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl">
+    {/* Close Icon */}
+    <div className="flex justify-end p-12">
+      <button onClick={()=>setIsMenuToggled(!isMenuToggled)}>
+    <XMarkIcon className="h-7 w-7 text-cyan hover:text-primary-500"/>
+      </button>
+    </div>
+    {/* MENU Items */}
+    <div className="ml-[33%] flex flex-col gap-6 text-1xl">
+      <Link
+        page="Home"
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+      <Link
+        page="About"
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+      <Link
+        page="Services"
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+      <Link
+        page="Contact us"
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+    </div>
+  </div>
+  )}
     </nav>
   );
 };
