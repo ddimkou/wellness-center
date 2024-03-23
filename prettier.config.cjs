08cb4eb328579c8d011cf658b3f96be7a824f81e
ddimkou <ddimkou@outlook.com>
2024-03-23 12:04:46 +0200

    navbar complete

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,3 +1,10 @@
-module.exports = {
-  plugins: [require("prettier-plugin-tailwindcss")],
-};
+// module.exports = {
+//   plugins: [require("prettier-plugin-tailwindcss")],
+// };
+
+async function loadTailwindCSSPrettierPlugin() {
+  const module = await import('prettier-plugin-tailwindcss');
+  return module.default; // Assuming the plugin is exported as default, adjust if necessary
+}
+
+module.exports = loadTailwindCSSPrettierPlugin();
--- src/scenes/Navbar/index.tsx
+++ src/scenes/Navbar/index.tsx
@@ -6,57 +6,67 @@ import useMediaQuery from "@/hooks/useMediaQuery";
 import { useState } from "react";
 import ActionButton from "@/shared/ActionButton";
 type Props = {
+  isTopOfPage: boolean;
   selectedPage: SelectedPage;
   setSelectedPage: (value: SelectedPage) => void;
 };
-const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
+const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
   const flexBetween = "flex items-center justify-between";
-  const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false)
-  const isAboveMediumScreens =useMediaQuery("(min-width:1060px)")
+  const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
+  const isAboveMediumScreens = useMediaQuery("(min-width:1060px)");
+  const navbarBackGround = isTopOfPage ? "" : "bg-primary-100 drop-shadow";
   return (
     <nav>
-      <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+      <div
+        className={`${navbarBackGround} ${flexBetween} fixed top-0 z-30 w-full py-6`}
+      >
         <div className={`${flexBetween} mx-auto w-5/6`}>
           <div className={`${flexBetween} w-full gap-16`}>
             {/* left side */}
             <img src={Logo} alt="logo" className="w-20 h-auto" />
             {/* right side */}
 
-            {isAboveMediumScreens ? (<div className={`${flexBetween} w-full`}>
-              {/* links */}
-              <div className={`${flexBetween} gap-8 text-sm`}>
-                <Link
-                  page="Home"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
-                <Link
-                  page="About"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
-                <Link
-                  page="Services"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
-                <Link
-                  page="Contact us"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
+            {isAboveMediumScreens ? (
+              <div className={`${flexBetween} w-full`}>
+                {/* links */}
+                <div className={`${flexBetween} gap-8 text-sm`}>
+                  <Link
+                    page="Home"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="About"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Services"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Contact us"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                </div>
+                {/* sign-in */}
+                <div className={`${flexBetween} gap-8 `}>
+                  <p>Sign In</p>
+                  <ActionButton setSelectedPage={setSelectedPage}>
+                    Reservation
+                  </ActionButton>
+                </div>
               </div>
-              {/* sign-in */}
-              <div className={`${flexBetween} gap-8 `}>
-                <p>Sign In</p>
-                <ActionButton setSelectedPage={setSelectedPage}>Reservation</ActionButton>
-              </div>
-            </div>) :
-            // mobile screen
-             (
-             <button className="rounded-full bg-cyan hover:bg-primary-500" onClick={()=>setIsMenuToggled(!isMenuToggled)}>
-              <Bars3Icon className="h-7 w-7 p-1 text-white"/>
-             </button>
+            ) : (
+              // mobile screen
+              <button
+                className="rounded-full bg-cyan hover:bg-primary-500"
+                onClick={() => setIsMenuToggled(!isMenuToggled)}
+              >
+                <Bars3Icon className="h-7 w-7 p-1 text-white" />
+              </button>
             )}
           </div>
         </div>
@@ -64,37 +74,37 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
       {/* MOBILE MENU toggle*/}
       {!isAboveMediumScreens && isMenuToggled && (
         <div className="fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl">
-    {/* Close Icon */}
-    <div className="flex justify-end p-12">
-      <button onClick={()=>setIsMenuToggled(!isMenuToggled)}>
-    <XMarkIcon className="h-7 w-7 text-cyan hover:text-primary-500"/>
-      </button>
-    </div>
-    {/* MENU Items */}
-    <div className="ml-[33%] flex flex-col gap-6 text-1xl">
-      <Link
-        page="Home"
-        selectedPage={selectedPage}
-        setSelectedPage={setSelectedPage}
-      />
-      <Link
-        page="About"
-        selectedPage={selectedPage}
-        setSelectedPage={setSelectedPage}
-      />
-      <Link
-        page="Services"
-        selectedPage={selectedPage}
-        setSelectedPage={setSelectedPage}
-      />
-      <Link
-        page="Contact us"
-        selectedPage={selectedPage}
-        setSelectedPage={setSelectedPage}
-      />
-    </div>
-  </div>
-  )}
+          {/* Close Icon */}
+          <div className="flex justify-end p-12">
+            <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+              <XMarkIcon className="h-7 w-7 text-cyan hover:text-primary-500" />
+            </button>
+          </div>
+          {/* MENU Items */}
+          <div className="ml-[33%] flex flex-col gap-6 text-1xl">
+            <Link
+              page="Home"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="About"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Services"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Contact us"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
