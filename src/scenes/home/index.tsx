7e9fb6d376efe3180bc0d1dbae7582ee679c519f
ddimkou <ddimkou@outlook.com>
2024-03-24 23:55:23 +0200

    home images/stylings

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -1,8 +1,12 @@
 import useMediaQuery from "@/hooks/useMediaQuery";
 import ActionButton from "@/shared/ActionButton";
 import { SelectedPage } from "@/shared/types";
-import banner from "@/assets/banner.png";
 import AnchorLink from "react-anchor-link-smooth-scroll";
+import banner from "@/assets/banner.png";
+import eqBanner2 from "@/assets/eqBanner2.png";
+import spons1 from "@/assets/spons1.png";
+import spons2 from "@/assets/spons2.png";
+import spons3 from "@/assets/spons3.png";
 ActionButton;
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
@@ -12,18 +16,17 @@ const Home = ({ setSelectedPage }: Props) => {
   const isAboveMediumScreens = useMediaQuery("(min-width:1060px)");
   return (
     <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
-      {/* tag & main header */}
-      <div>
+      {/* image & main header */}
+      <div className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6">
         {/* main header */}
-        <div>
+        <div className="z-10 mt-32 md:basis-3/5">
           {/* headings */}
-          <div>
+          <div className="md:-mt-20">
             <div>
-              <div>
-                <img src={banner} alt="Wellness Center Banner" />
-              </div>
+              <img src={banner} alt="Wellness Center Banner" />
             </div>
-            <p>
+
+            <p className="mt-8 text-sm">
               We believe in the holistic approach to wellness, integrating mind
               and body practices that extend beyond the massage table. Our
               commitment to your wellbeing is our utmost priority, making the
@@ -32,7 +35,7 @@ const Home = ({ setSelectedPage }: Props) => {
             </p>
           </div>
           {/* ACTIONS */}
-          <div>
+          <div className="mt-8 flex items-center gap-8 ">
             <ActionButton setSelectedPage={setSelectedPage}>
               JOIN NOW
             </ActionButton>
@@ -47,8 +50,27 @@ const Home = ({ setSelectedPage }: Props) => {
         </div>
 
         {/* image */}
-        <div></div>
+        <div className="flex basis-3/5 justify-center md:ml-40 md:mt-16 md:justify-items-end sm:mt-12">
+          <img
+            src={eqBanner2}
+            alt="home-pageGraphic"
+            className="w-[20rem] h-[20rem] "
+          />
+        </div>
       </div>
+
+      {/* Sponsors */}
+      {isAboveMediumScreens && (
+        <div className="h-[150px] w-full bg-primary-100 py-10">
+          <div className="mx-auto w-5/6">
+            <div className="flex w-full items-center justify-between gap-8 ">
+              <img src={spons1} alt="spons1" className="h-[60px] w-auto" />
+              <img src={spons2} alt="spons2" className="h-[60px] w-auto" />
+              <img src={spons3} alt="spons3" className="h-[60px] w-auto" />
+            </div>
+          </div>
+        </div>
+      )}
     </section>
   );
 };
