3966300b170b11bf7800510f8f51f9586608c897
ddimkou <ddimkou@outlook.com>
2024-03-28 00:18:46 +0200

    useForm and formspree.io

--- src/scenes/Contact/index.tsx
+++ src/scenes/Contact/index.tsx
@@ -7,7 +7,19 @@ import massageGraphic from "@/assets/massageGraphic.png";
 
 type Props = { setSelectedPage: (value: SelectedPage) => void };
 
+const inputStyles = ` mt-5 w-full rounded-lg bg-primary-300 px-5 py-3 placeholder-white`;
 const ContactUs = ({ setSelectedPage }: Props) => {
+  const {
+    register,
+    trigger,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = async (e: any) => {
+    const isValid = await trigger();
+    if (!isValid) {
+      e.preventDefault();
+    }
+  };
   return (
     <section id="contactus" className="mx-auto w-5/6 pt-24 pb-32">
       <motion.div
@@ -32,6 +44,82 @@ const ContactUs = ({ setSelectedPage }: Props) => {
             architecto fugit. Natus, ab architecto. Nobis.
           </p>
         </motion.div>
+        {/* form and img */}
+        <div className="mt-10 justify-between gap-8 md:flex">
+          <motion.div
+            className="mt-10 basis-3/5 md:mt-0"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, y: 50 },
+              visible: { opacity: 1, y: 0 },
+            }}
+          >
+            <form
+              target="_blank"
+              onSubmit={onSubmit}
+              method="POST"
+              action="https://formspree.io/f/xnqegpdp"
+            >
+              <input
+                className={inputStyles}
+                type="text"
+                placeholder="Name"
+                {...register("name", {
+                  required: true,
+                  maxLength: 50,
+                })}
+              />
+              {errors.name && (
+                <p className="mt-1 text-primary-500">
+                  {errors.name.type === "required" && "This field is required"}
+                  {errors.name.type === "maxLength" && "Max length is 50 chars"}
+                </p>
+              )}
+              <input
+                className={inputStyles}
+                type="text"
+                placeholder="Email"
+                {...register("email", {
+                  required: true,
+                  pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/i,
+                })}
+              />
+              {errors.email && (
+                <p className="mt-1 text-primary-500">
+                  {errors.email.type === "required" && "This field is required"}
+                  {errors.email.type === "pattern" && "Invalid Email adress"}
+                </p>
+              )}
+              <textarea
+                className={inputStyles}
+                rows={4}
+                cols={50}
+                placeholder="Message"
+                {...register("message", {
+                  required: true,
+                  maxLength: 2000,
+                })}
+              />
+              {errors.message && (
+                <p className="mt-1 text-primary-500">
+                  {errors.message.type === "required" &&
+                    "This field is required"}
+                  {errors.message.type === "maxLength" &&
+                    "Max length is 2000 chars"}
+                </p>
+              )}
+              <button
+                type="submit"
+                className="mt-5 rounded-lg bg-secondary-500 px-20 py-3 transition duration-500 hover:text-white"
+              >
+                Submit
+              </button>
+            </form>
+          </motion.div>
+        </div>
       </motion.div>
     </section>
   );
