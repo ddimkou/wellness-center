92ff0a26cc1140f3f3d36d9672419f7b6f5413e4
ddimkou <ddimkou@outlook.com>
2024-03-27 16:13:47 +0200

    about: motion

--- src/scenes/About/index.tsx
+++ src/scenes/About/index.tsx
@@ -7,6 +7,9 @@ import { SelectedPage, BenefitType } from "@/shared/types";
 import { motion } from "framer-motion";
 import HText from "@/shared/HText";
 import Benefit from "./Benefit";
+import ActionButton from "@/shared/ActionButton";
+// import SparklesImage from "@/assets/Sparkles.png";
+import massageImg from "@/assets/massageImg.png";
 
 const benefits: BenefitType[] = [
   {
@@ -77,6 +80,69 @@ const About = ({ setSelectedPage }: Props) => {
             />
           ))}
         </motion.div>
+
+        {/* GRAPHICS AND DESCRIPTION */}
+        <div className=" items-center justify-between gap-20 md:mt-28 md:flex  ">
+          {/* graphic */}
+          <img
+            src={massageImg}
+            alt="about-page-graphics"
+            className="my-16 mx-auto h-[250px] "
+          />
+          {/* description */}
+          <div>
+            {/* title */}
+            <div className="relative">
+              <div className="before:absolute before:-top-20 before:left-20 before:z-[-1] before:content-abstractwaves">
+                <motion.div
+                  initial="hidden"
+                  whileInView="visible"
+                  viewport={{ once: true, amount: 0.5 }}
+                  transition={{ duration: 0.5 }}
+                  variants={{
+                    hidden: { opacity: 0, x: 50 },
+                    visible: { opacity: 1, x: 0 },
+                  }}
+                >
+                  <HText> MILLIONS OF HAPPY CUSTOMERS</HText>
+                </motion.div>
+              </div>
+            </div>
+
+            {/* description */}
+            <motion.div
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.5 }}
+              transition={{ delay: 0.2, duration: 0.5 }}
+              variants={{
+                hidden: { opacity: 0, x: -50 },
+                visible: { opacity: 1, x: 0 },
+              }}
+            >
+              <p className="my-5">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
+                dolorum est iure debitis in tenetur! Lorem ipsum dolor, sit amet
+                consectetur adipisicing elit. Enim, consequuntur exercitationem
+                voluptatem deleniti nostrum dolorum! Lorem, ipsum dolor sit amet
+                consectetur adipisicing elit. Animi minima, possimus quas nulla
+                aliquam fugiat.
+              </p>
+              <p className="mb-5">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Ducimus beatae repudiandae omnis, eligendi numquam quaerat vitae
+                voluptates? Exercitationem incidunt doloremque mollitia ex.
+              </p>
+            </motion.div>
+            {/* button */}
+            <div className="relative mt-16">
+              <div className="before:absolute before:-bottom-20 before:right-40 before:z-[-1] before:content-sparkles"></div>
+              <ActionButton setSelectedPage={setSelectedPage}>
+                JOIN NOW
+              </ActionButton>
+            </div>
+          </div>
+        </div>
       </motion.div>
     </section>
   );
